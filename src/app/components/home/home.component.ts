ef424c13a0353ce4004753bebe4955e75d8182a3
GelenMR <gelen.montilla.r@gmail.com>
2019-08-20 00:19:57 -0400

    Se agregan Estilos y clases para hacer pagina responsiva, posicionamiento de logo y otros elementos del home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  title = 'Gelen Montilla';
+  title = 'FrontEnd Developer';
   constructor() { }
 
   ngOnInit() {
